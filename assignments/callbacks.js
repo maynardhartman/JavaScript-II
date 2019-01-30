727bd0314f542da429eda9dba9bf5c50f8f20863
Maynard J. Hartman, Jr <mjhartman@brighthouse.com>
2019-01-30 04:42:25 -0500

    Add files via upload

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -24,25 +24,58 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb(arr); 
 }
+
+function finished(arr){
+  console.log(arr.length);
+}
+
+getLength(items, finished);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[3]);
 }
+
+function finishedLast(lastItem) {
+  console.log(lastItem); 
+}
+last(items, finishedLast);
+
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
 }
+
+function finishedSums(result) {
+  console.log(result);
+}
+sumNums(4, 6, finishedSums);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb( x * y );
 }
+
+function finishedMulti(product){
+  console.log(product);
+}
+multiplyNums(4, 6, finishedMulti);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  list.filter (item => list);
+  cb(true);
 }
 
+function finishedContains(answer){
+  console.log(answer);
+}
+
+contains("Gun", items, finishedContains);
 /* STRETCH PROBLEM */
 
 function removeDuplicates(array, cb) {
